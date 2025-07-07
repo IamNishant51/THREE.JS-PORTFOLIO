58000298155ec1704010fc79c1bdd254bfb002ec
IamNishant51 <anonymouslucifer400@gmail.com>
2025-07-07 17:45:45 +0530

    in nav bar menue button done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import Navbar from "./sections/Navbar"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="relative w-screen min-h-screen overflow-x-auto">
+      <Navbar/>
+      <section className="min-h-screen"/>
+      <section className="min-h-screen"/>
+    </div>
   )
 }
 
